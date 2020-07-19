405319352b02275f66a4b10d01e30d049b4c8dd6
chcloudcode <light_fighting@163.com>
2020-07-19 10:26:59 +0800

    finish week04

--- week04/NOTE.md
+++ week04/NOTE.md
@@ -1,1 +1,17 @@
 学习笔记
+
+## 相关资料
+- pandas中文网:[https://www.pypandas.cn/](https://www.pypandas.cn/)
+- pandas官方文档：[https://pandas.pydata.org/docs/reference/index.html](https://pandas.pydata.org/docs/reference/index.html)
+- numpy学习文档：[https://numpy.org/doc/](https://numpy.org/doc/)
+- matplotlib学习文档：[https://matplotlib.org/contents.html](https://matplotlib.org/contents.html)
+
+## 小结
+
+pandas常用操作动都可以查询文档得到
+
+记录：
+- nunique 类似 mysql COUNT(DISTINCT order_id) 操作 
+- 产生指定数量、范围的随机数：np.random.randint(0, len(group), 10)]
+
+
---
+++ week04/SqlToPandas.py
@@ -0,0 +1,72 @@
+# -*- coding:utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('data.csv')
+df.columns = ['no', 'name', 'age', 'character', 'motto']
+
+# case1
+print("SELECT * FROM data;")
+print(df)
+print("\n")
+
+# case2
+print("SELECT * FROM data LIMIT 10")
+print(df[0:10])
+print("\n")
+
+# case3
+print("SELECT name FROM data;")
+print(df['name'])
+print("\n")
+
+# case4
+print("SELECT COUNT(name) FROM data;")
+print(df['name'].count())
+print("\n")
+
+# case5
+print("SELECT * FROM data WHERE no>6 AND age<=30")
+print(df[(df['no'] > 6) & (df['age'] <= 30)])
+print("\n")
+
+# case6
+print("SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;")
+df2 = pd.DataFrame([
+    {'id': '10010', 'name': 'xiaoming', 'order_id': '11111'},
+    {'id': '10010', 'name': 'xiaohong', 'order_id': '11111'},
+    {'id': '10010', 'name': 'xiaoqiang', 'order_id': '11112'},
+    {'id': '10011', 'name': 'xiaoli', 'order_id': '11112'},
+    {'id': '10011', 'name': 'xiaowang', 'order_id': '11113'},
+    {'id': '10011', 'name': 'xiaozhao', 'order_id': '11113'}
+])
+print(df2.groupby('id').aggregate({'order_id': 'nunique'}))
+print("\n")
+
+# case7
+print("SELECT * FROM table1 t1 INNER JOIN table2 t2 ON t1.id = t2.id;")
+group = ['a', 'b', 'c']
+table1 = pd.DataFrame({
+    "group": [group[x] for x in np.random.randint(0, len(group), 10)],
+    "age": np.random.randint(15, 50, 10)
+})
+
+table2 = pd.DataFrame({
+    "group": [group[x] for x in np.random.randint(0, len(group), 10)],
+    "salary": np.random.randint(5, 50, 10),
+})
+print(pd.merge(table1, table2, on='group', how='inner'))
+
+# case8
+print("SELECT * FROM table1 UNION SELECT * FROM table2;")
+print(pd.concat([table1, table2]))
+
+# case9
+print("DELETE FROM table1 WHERE id=10;")
+delete_index = df[df['no'] == 10].index
+print(df.drop(delete_index, axis=0))
+
+# case 10
+print("ALTER TABLE table1 DROP COLUMN column_name;")
+print(df.drop('no', axis=1))
